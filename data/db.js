e7f73f9c9e86769ea729adb030c728fcf68e79d8
oninotna <antoninomusarratubbi@gmail.com>
2026-01-03 11:57:53 +0100

    refactor pool + controllers

--- data/db.js
+++ data/db.js
@@ -1,14 +1,29 @@
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: process.env.HOST_DB,
+//   user: process.env.USER_DB,
+//   port: process.env.DB_PORT,
+//   password: process.env.PASS_DB,
+//   database: process.env.DB_NAME,
+// });
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log("Connected to MySQL!");
+// });
+// module.exports = connection;
+
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
   host: process.env.HOST_DB,
   user: process.env.USER_DB,
   port: process.env.DB_PORT,
   password: process.env.PASS_DB,
   database: process.env.DB_NAME,
+  connectionLimit: 3,
+  waitForConnections: true,
+  enableKeepAlive: true,
 });
-connection.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to MySQL!");
-});
-module.exports = connection;
+
+module.exports = pool;
